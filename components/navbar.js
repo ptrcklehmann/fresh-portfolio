11c356bae5d262768995ee8f4e0c96837d92332b
patrick lehmann <ptrcklehmann@gmail.com>
2022-05-23 22:47:49 +0200

    add new logo and favicons

--- components/navbar.js
+++ components/navbar.js
@@ -2,6 +2,9 @@ import { Fragment } from 'react'
 import { Disclosure } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import ActiveLink from './active-link'
+import logo from '../public/images/logo.svg'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const navigation = [
   { name: 'Home', href: '/', current: false },
@@ -9,12 +12,15 @@ const navigation = [
   { name: 'Contact', href: '/contact', current: false },
 ]
 export default function Navbar() {
+  const staticlLoader = () => {
+    return `${logo.href}?w=${75}&q=${75}`
+  }
   return (
     <Disclosure as='nav'>
       {({ open }) => (
         <>
-          <div className='px-2 mx-auto max-w-7xl sm:px-6 lg:px-8'>
-            <div className='relative flex items-center justify-between h-16'>
+          <div className='px-2 mx-auto mt-5 max-w-7xl sm:px-6 lg:px-5'>
+            <div className='relative flex items-end justify-between h-16'>
               <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
                 {/* Mobile menu button*/}
                 <Disclosure.Button className='inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
@@ -26,18 +32,20 @@ export default function Navbar() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className='flex items-center justify-center flex-1 sm:items-stretch sm:justify-between'>
-                <div className='flex items-center flex-shrink-0'>
-                  <img
-                    className='block w-auto h-8 lg:hidden'
-                    src='https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg'
-                    alt='Workflow'
-                  />
-                  <img
-                    className='hidden w-auto h-8 lg:block'
-                    src='https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg'
-                    alt='Workflow'
-                  />
+              <div className='flex items-end justify-center flex-1 sm:items-stretch sm:justify-between'>
+                <div className='flex items-end flex-shrink-0'>
+                  <Link href='/'>
+                    <a href='/' alt='Home' className='p-0 m-0 leading-none'>
+                      <Image
+                        src='/images/logo.svg'
+                        width={50}
+                        height={50}
+                        layout='intrinsic'
+                        alt='ptrcklehmann.com'
+                        className='hidden w-auto h-8 mr-auto lg:block'
+                      />
+                    </a>
+                  </Link>
                 </div>
                 <div className='self-end hidden sm:block sm:ml-6'>
                   <div className='flex space-x-4'>
@@ -47,7 +55,11 @@ export default function Navbar() {
                         activeClassName='text-white transition-all rounded-md bg-gradient-to-t from-accent-7 via-accent-8 to-accent-9'
                         className='text-white transition-all rounded-md bg-gradient-to-t from-accent-7 via-accent-8 to-accent-9 hover:bg-accent-2 hover:text-type-1'
                       >
-                        <a key={item.name} href={item.href} className='px-3 py-2 text-sm font-medium rounded-md'>
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className='px-3 py-2 text-sm font-medium rounded-md'
+                        >
                           {item.name}
                         </a>
                       </ActiveLink>
@@ -65,7 +77,12 @@ export default function Navbar() {
                   href={item.href}
                   className='px-3 py-2 text-sm font-medium text-transparent transition-all rounded-md bg-clip-text bg-gradient-to-t from-accent-7 via-accent-8 to-accent-9 hover:bg-accent-2 hover:text-type-1'
                 >
-                  <Disclosure.Button key={item.name} as='a' href={item.href} className='px-3 py-2 text-sm font-medium rounded-md'>
+                  <Disclosure.Button
+                    key={item.name}
+                    as='a'
+                    href={item.href}
+                    className='px-3 py-2 text-sm font-medium rounded-md'
+                  >
                     {item.name}
                   </Disclosure.Button>
                 </ActiveLink>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    images: {
-        loader: 'custom',
-    },
+    // images: {
+    //     loader: 'custom',
+    // },
     optimizeFonts: false,
 }
